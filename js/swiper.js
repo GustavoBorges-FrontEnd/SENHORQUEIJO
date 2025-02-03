6a29a58e4aba444b890076f9f979eabcb12ee365
Gustavo Borges <guborges.ggb@gmail.com>
2025-02-03 19:34:15 -0300

    Atualizando o css

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,10 @@
 
 var swiper = new Swiper(".mySwiper", {
+    loop: true, 
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false, 
+        },
     direction: "vertical",
     pagination: {
     el: ".swiper-pagination",
