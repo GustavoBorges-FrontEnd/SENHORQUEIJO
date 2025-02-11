dae3c48d19f9a5fc54ce54d71247dce81aaeb6c6
Gustavo Borges <guborges.ggb@gmail.com>
2025-02-11 20:26:54 -0300

    Adicionando algumas funções

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,19 @@
+function toggleDropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.btnfilter')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 // Função para filtrar produtos por categoria
 function filtrarProdutos(categoria) {
     // Seleciona todos os cards de produtos
